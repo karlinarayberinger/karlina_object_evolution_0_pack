bc30d6ea27bf2dc332cfed79b2ab7e208f43844d
karbytes <karlina.ray.beringer@protonmail.com>
2023-02-16 22:00:14 -0800

    Add files via upload

--- riemann_sum.js
+++ riemann_sum.js
@@ -102,6 +102,176 @@ function generate_blank_canvas() {
 }
 
 /**
+ */
+/*
+function draw_cartesian_grid_on_canvas() {
+    try {
+        let canvas = document.getElementById("rectangular_canvas");
+        let context = canvas.getContext("2d");
+        const canvas_width = canvas.width;
+        const canvas_height = canvas.height;
+        const grid_unit_square_length = 25;
+        const number_of_horizontal_grid_line_segments = Math.floor(canvas_width / grid_unit_square_length);
+        const number_of_vertical_grid_line_segments = Math.floor(canvas_height / grid_unit_square_length);
+    }
+    catch(exception) {
+        console.log("An exception to normal functioning occurred during the runtime of draw_cartesian_grid_on_canvas(): " + exception);
+    }
+}*/
+
+/**
+ */
+function draw_cartesian_grid_for_f0() {
+
+}
+
+/**
+ */
+function draw_cartesian_grid_for_f1() {
+
+}
+
+/**
+ */
+function draw_cartesian_grid_for_f2() {
+
+}
+
+function test() {
+    var grid_size = 25;
+    var x_axis_distance_grid_lines = 5;
+    var y_axis_distance_grid_lines = 5;
+    var x_axis_starting_point = { number: 1, suffix: '\u03a0' };
+    var y_axis_starting_point = { number: 1, suffix: '' };
+
+    var canvas = document.getElementById("rectangular_canvas");
+    var ctx = canvas.getContext("2d");
+
+    var canvas_width = canvas.width;
+    var canvas_height = canvas.height;
+
+    var num_lines_x = Math.floor(canvas_height/grid_size);
+    var num_lines_y = Math.floor(canvas_width/grid_size);
+
+    // Draw grid lines along X-axis
+    for(var i=0; i<=num_lines_x; i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+
+        // If line represents X-axis draw in different color
+        if(i == x_axis_distance_grid_lines) 
+            ctx.strokeStyle = "#000000";
+        else
+            ctx.strokeStyle = "#e9e9e9";
+
+        if(i == num_lines_x) {
+            ctx.moveTo(0, grid_size*i);
+            ctx.lineTo(canvas_width, grid_size*i);
+        }
+        else {
+            ctx.moveTo(0, grid_size*i+0.5);
+            ctx.lineTo(canvas_width, grid_size*i+0.5);
+        }
+        ctx.stroke();
+}
+
+// Draw grid lines along Y-axis
+for(i=0; i<=num_lines_y; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    
+    // If line represents X-axis draw in different color
+    if(i == y_axis_distance_grid_lines) 
+        ctx.strokeStyle = "#000000";
+    else
+        ctx.strokeStyle = "#e9e9e9";
+    
+    if(i == num_lines_y) {
+        ctx.moveTo(grid_size*i, 0);
+        ctx.lineTo(grid_size*i, canvas_height);
+    }
+    else {
+        ctx.moveTo(grid_size*i+0.5, 0);
+        ctx.lineTo(grid_size*i+0.5, canvas_height);
+    }
+    ctx.stroke();
+}
+
+// Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+ctx.translate(y_axis_distance_grid_lines*grid_size, x_axis_distance_grid_lines*grid_size);
+
+// Ticks marks along the positive X-axis
+for(i=1; i<(num_lines_y - y_axis_distance_grid_lines); i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(grid_size*i+0.5, -3);
+    ctx.lineTo(grid_size*i+0.5, 3);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(x_axis_starting_point.number*i + x_axis_starting_point.suffix, grid_size*i-2, 15);
+}
+
+// Ticks marks along the negative X-axis
+for(i=1; i<y_axis_distance_grid_lines; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-grid_size*i+0.5, -3);
+    ctx.lineTo(-grid_size*i+0.5, 3);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'end';
+    ctx.fillText(-x_axis_starting_point.number*i + x_axis_starting_point.suffix, -grid_size*i+3, 15);
+}
+
+// Ticks marks along the positive Y-axis
+// Positive Y-axis of graph is negative Y-axis of the canvas
+for(i=1; i<(num_lines_x - x_axis_distance_grid_lines); i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-3, grid_size*i+0.5);
+    ctx.lineTo(3, grid_size*i+0.5);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(-y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, grid_size*i+3);
+}
+
+// Ticks marks along the negative Y-axis
+// Negative Y-axis of graph is positive Y-axis of the canvas
+for(i=1; i<x_axis_distance_grid_lines; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-3, -grid_size*i+0.5);
+    ctx.lineTo(3, -grid_size*i+0.5);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, -grid_size*i+3);
+}
+}
+
+/**
  * Draw a line segment whose thickness is one pixel and whose color is black from of the left edge of the HTML canvas 
  * whose id is "rectangular_canvas" on the web page defined by riemann_sum.html to the right edge of that canvas and 
  * such that the line segment drawn by this function is parallel to the top and bottom edges of that canvas.
@@ -159,8 +329,9 @@ function initialize_application() {
     try {
         let output_div = document.getElementById("output");
         output_div.innerHTML = generate_blank_canvas();
-        draw_vertical_cartesian_plane_axis();
-        draw_horizontal_cartesian_plane_axis();
+        // draw_vertical_cartesian_plane_axis();
+        // draw_horizontal_cartesian_plane_axis();
+        test();
         populate_f_menu();
         populate_p_menu();
         populate_r_menu();
