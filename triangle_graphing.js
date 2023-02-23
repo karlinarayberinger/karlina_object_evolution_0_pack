0ac5414c1dffd2d2a175616d71c2114117c5a475
karbytes <karlina.ray.beringer@protonmail.com>
2023-02-22 20:57:06 -0800

    Add files via upload

--- triangle_graphing.js
+++ triangle_graphing.js
@@ -462,7 +462,7 @@ function generate_triangle_using_input_coordinates() {
         select_menu_container_paragraph.innerHTML = ('C.X := ' + selected_menu_option_value + '. // horizontal position of two-dimensional POINT labeled C.');
         // Store the selected menu option in a variable to be used later as its corresponding POINT property (as the property labeled X of the object labeled C).
         x_coordinate_value = selected_menu_option_value; 
-        // Transform the second input select menu (for C.Y) into plain text displaying its selected option.
+        // Transform the sixth input select menu (for C.Y) into plain text displaying its selected option.
         select_menu_container_paragraph = document.getElementById("c_y_menu_container");
         selected_menu_option_value = parseInt(get_selected_menu_option_value("c_y_menu"));
         select_menu_container_paragraph.innerHTML = ('C.Y := ' + selected_menu_option_value + '. // vertical position of two-dimensional POINT labeled C.'); 
