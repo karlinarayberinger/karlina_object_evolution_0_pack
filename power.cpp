fd96375c698dda92cbf15d5cc1713e4005662e68
karbytes <karlina.ray.beringer@protonmail.com>
2023-05-13 16:40:25 -0700

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,35 @@
+/**
+ * file: power.cpp
+ * type: C++ (source file)
+ * date: 13_MAY_2023
+ * author: karbytes
+ * license: PUBLIC_DOMAIN
+ */
+
+/* preprocessing directives */
+#include <iostream> // command line input and command line output operations
+#include <fstream> // file input and file output operations
+
+/*
+#include <cmath> // power function (e.g. 8 = pow(2,3))
+#define MAXIMUM_N 65 // constant which represents maximum N value
+*/
+
+/* function prototypes */
+double power(double base, double exponent);
+
+/**
+ * Program Entry Point 
+ */
+int main() 
+{
+    // Program Exit Point
+    return 0;
+}
+
+/**
+ */
+double power(double base, double exponent) 
+{
+    return 0.0;
+}
