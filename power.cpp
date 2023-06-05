326339d007f33d7958d3f79b1eb6340499a52711
karbytes <karlina.ray.beringer@protonmail.com>
2023-06-05 03:38:56 -0700

    Add files via upload

--- power.cpp
+++ power.cpp
@@ -1,7 +1,7 @@
 /**
  * file: power.cpp
  * type: C++ (source file)
- * date: 03_JUNE_2023
+ * date: 05_JUNE_2023
  * author: karbytes
  * license: PUBLIC_DOMAIN
  */
@@ -10,15 +10,13 @@
 #include <iostream> // command line input and command line output operations
 #include <fstream> // file input and file output operations
 
-// remove later
-#include <cmath>
-
-/*
-#include <cmath> // power function (e.g. 8 = pow(2,3))
-#define MAXIMUM_N 65 // constant which represents maximum N value
-*/
-
 /* function prototypes */
+unsigned long long int compute_factorial_of_N_using_iteration(int N);
+long double eulers_number_approximation(int N);
+long double log(float x);
+double exp(float x);
+
+
 bool is_whole_number(double input);
 double absolute_value(double input);
 double power(double base, double exponent);
@@ -34,6 +32,79 @@ int main()
     std::cout << "\n\npow(2, 0.5) := " << pow(2, 0.5) << ".";
     // Program Exit Point
     return 0;
+}
+
+/**
+ * Compute N factorial (N!) using an iterative algorithm.
+ * 
+ * If N is a natural number, then N! is the product of exactly one instance 
+ * of each unique natural number which is less than or equal to N. 
+ * N! := N * (N - 1) * (N - 2) * (N - 3) * ... * 3 * 2 * 1. 
+ * 
+ * If N is zero, then N! is one.
+ * 0! := 1.
+ * 
+ * The value returned by this function is a nonnegative integer (which is N factorial).
+ * 
+ * Assume that N is a nonnegative integer no larger than 65.
+ */
+unsigned long long int compute_factorial_of_N_using_iteration(int N)
+{
+    int i = 0;
+    unsigned long long int F = 0; 
+    i = ((N >= 0) && (N <= 65)) ? N : 0; 
+    F = (N > 0) ? N : 1; 
+    while (i > 0) 
+    {
+        if (i > 1) F *= (i - 1); 
+        i -= 1; 
+    }
+    return F;
+}
+
+/**
+ * Generate an approximation of the the mathematical constant named e (i.e. Euler’s Number).
+ * 
+ * N is assumed to be a natural number.
+ * 
+ * The approximate value of Euler's Number is computed by adding N unique terms 
+ * (and each of those N terms is 1 divided by factorial n 
+ * (and n is a nonnegative integer which is smaller than or equal to N)).
+ * 
+ * Note that the actual value of e is a limit which cannot be determined using 
+ * a finite number of additions as described in the previous sentence. Instead, 
+ * the actual value of e is defined as the sum of every unique instance of 
+ * (1/(n!)) such that n is a nonnegative integer.
+ * 
+ * The value returned by this function is a positive floating-point number (which is the Nth approximation of Euler's Number).
+ * 
+ * Assume that N is a nonnegative integer no larger than 65.
+ */
+long double eulers_number_approximation(int N)
+{
+    long double A = 1.0;
+    int i = 0;
+    unsigned long long int * T;
+    if ((N < 0) || (N > MAXIMUM_N)) N = 1;
+    T = new unsigned long long int [N];
+    for (i = 0; i < N; i++) T[i] = N - i;
+    for (i = 0; i < N; i += 1) T[i] = compute_factorial_of_N_using_iteration(T[i]);
+    for (i = 0; i < N; i += 1) A += (long double) 1 / T[i];
+    delete [] T;
+    return A;
+}
+
+/**
+ */
+long double log(double x) 
+{
+    long double e = eulers_number_approximation(65);
+    long double log_x = 0; 
+    while(x > e){
+        log_X = 1;
+        x /= e;
+    }
+    return log_x;
 }
 
 /**
